b9feb436f4859de9682e3ed4adbad634ee2208c7
MariaABurmistrova <mary.18@bk.ru>
2023-11-18 19:08:32 +0300

    Initial commit

---
+++ configuration.py
@@ -0,0 +1,9 @@
+'''
+Yandex Practicum        Дипломная работа
+Бурмистрова Мария       Когорта 10
+18 ноября 2023 г.
+'''
+
+URL_SERVICE = "https://06b42863-1c7f-4274-95d7-d38b0a4e8bb9.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_INFO = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,19 @@
+'''
+Yandex Practicum        Дипломная работа
+Бурмистрова Мария       Когорта 10
+18 ноября 2023 г.
+'''
+
+order_body = {
+    "firstName": "Иван",
+    "lastName": "Иванович",
+    "address": "Москва, проспект Мира, 38",
+    "metroStation": 99,
+    "phone": "+79993456789",
+    "rentTime": 3,
+    "deliveryDate": "2023-06-05",
+    "comment": "Позвоните за час до прибытия",
+    "color": [
+        "BLACK"
+    ]
+}
